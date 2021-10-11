8dd8ece44ad57b94fc8e128df56551aee906c57d
DESKTOP-9TELULQ\kpj17 <kaitlin.p.johnson@ndus.edu>
2021-10-10 20:49:50 -0500

    initial commit

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Line comment
+
+/* Block comment:
+ - Form1.cs is the functionality part
+ - Form1.cs [Desgin] is the design part
+ 
+ 
+ Video Lecture 1: made it to 25:00    :-0
+ */
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            // Inititialize the form i.e. what they will see when the form starts up
+            InitializeComponent();
+
+            // Everything after form initialization will be what happens after the form is opened.
+
+        }
+    }
+}
